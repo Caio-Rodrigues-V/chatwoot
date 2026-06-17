af635d238d5f81532e5c72c69c86b642f8499a9e
Caio Vicente <144162954+Caio-Rodrigues-V@users.noreply.github.com>
2026-06-17 13:03:24 -0300

    Skip bot access token validation for inbox messages

--- app/controllers/api/v1/accounts/inbox_messages_controller.rb
+++ app/controllers/api/v1/accounts/inbox_messages_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::Accounts::InboxMessagesController < Api::V1::Accounts::BaseContro
   skip_before_action :authenticate_user!, raise: false
   skip_before_action :authenticate_access_token!, raise: false
   skip_before_action :check_subscription, raise: false
+  skip_before_action :validate_bot_access_token!, raise: false
 
   before_action :set_inbox
   before_action :verify_message_event
